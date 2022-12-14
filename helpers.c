50a57303963fff5959996d162494226d2b35130d
robert-todora <roberttodora@gmail.com>
2022-12-13 23:08:32 -0500

    added readfile function

--- helpers.c
+++ helpers.c
@@ -1,5 +1,9 @@
-#include <stdio.h>
 #include "helpers.h"
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 unsigned int xorbuf(unsigned int *buffer, int size) {
     unsigned int result = 0;
@@ -9,3 +13,21 @@ unsigned int xorbuf(unsigned int *buffer, int size) {
     return result;
 }
 
+void readFile(char* filename, int blocksize, int blockcount){ 
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0) { 
+        printf("Error opening file %s\n", filename); 
+    }
+    else {
+        unsigned int xorvalue = 0;
+        unsigned int buffer[blocksize];
+        for(int i = 0; i < blockcount; i++) { 
+            int current_read = read(fd, buffer, blocksize);
+            xorvalue ^= xorbuf(buffer, current_read);
+        }
+        printf("%x\n", xorvalue);
+    }
+    if(close(fd) != 0) { 
+        printf("Error closing file");
+    }
+}
