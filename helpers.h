57d8f3dd9b82342cbe1fad5fc40e37175b0db1b9
robert-todora <roberttodora@gmail.com>
2022-12-18 23:00:06 -0500

    parts 1 and 2 completed

--- helpers.h
+++ helpers.h
@@ -3,5 +3,3 @@ unsigned int xorbuf(unsigned int *buffer, int size);
 void readFile(char* filename, unsigned int blocksize, unsigned int blockcount);
 int readFile_run2(char* filename, unsigned int blocksize);
 void writeFile(char* filename, unsigned int blocksize, unsigned int blockcount);
-unsigned int computeBlockCt(char* filename, unsigned int blocksize); 
-size_t fize(const char* filename); 
